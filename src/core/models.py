9944a7433f9d93dc212dee58af57263bfacc34d1
Paolo <paolo@potatolondon.com>
2018-03-02 17:04:35 +0000

    Create company model [touch: 20]

--- src/core/models.py
+++ src/core/models.py
@@ -1,5 +1,24 @@
 from djangae.contrib.gauth_datastore.models import GaeAbstractDatastoreUser
+from django.db import models
+from  django.utils import timezone
+import hashlib
+import datetime
 
 
 class User(GaeAbstractDatastoreUser):
     pass
+
+class Company(models.Model):
+    uuid = models.CharField(unique=True, editable=False)
+    company_name= models.CharField(max_length=50)
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.created_at = timezone.now()
+            m = hashlib.md5()
+            md5 = m.update(self.company_name + self.created_at.isoformat())
+            self.uuid = m.hexdigest()
+        super(Company, self).save(*args, **kwargs)
