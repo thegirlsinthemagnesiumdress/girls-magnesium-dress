e71b19942f33e2c4b89d58879e7406518ce665d4
Paolo <paolo@potatolondon.com>
2018-03-06 12:14:40 +0000

    Add filterin by uid [touch: 20]

--- src/api/views.py
+++ src/api/views.py
@@ -1,35 +1,3 @@
-# from django.http import JsonResponse
-# import logging
-# from core.models import Company
-
-
-# # This is a view used to investigate and prove we can use qualtrics
-# # web services to populate the company name
-# #
-# # Everything is hardcoded. This has to be taken as a quick prototype.
-# def company(request):
-#     # Prototypy token auth
-#     token = 'vG9r2NgG6Azr'
-#     request_token = request.META.get('HTTP_X_API_TOKEN')
-#     logging.info('Request toke %s', request_token)
-
-#     # Getting this from query string and not from url pattern
-#     # because I can't find a way to create the right url from Qualtrics.
-#     company_id = request.GET.get('id')
-
-#     if token != request_token:
-#         return JsonResponse(status=403, data={'error': 'Forbidden'})
-
-#     if not company_id:
-#         return JsonResponse(status=404, data={'error': 'You need to provide company id'})
-
-#     res = Company.objects.filter(uid=company_id)
-
-#     if len(res) > 0:
-#         return JsonResponse({ 'results': [{'name': res[0].company_name}]})
-#     else:
-#         return JsonResponse(data={'results': []})
-
 from rest_framework import permissions, viewsets
 from api.serializers import SurveySerializer
 from core.models import Survey
@@ -37,7 +5,16 @@ from core.models import Survey
 class SurveyViewSet(viewsets.ModelViewSet):
     serializer_class = SurveySerializer
     queryset = Survey.objects.all()
-    # import pdb; pdb.set_trace()
-    # http_method_names=["get", "post"]
 
-
+    def get_queryset(self):
+        """
+        Returns the unique survey given his uid or an empty list.
+        We don't need to expose all the Surveys for now.
+        """
+        queryset = Survey.objects.all()
+        uid = self.request.query_params.get('uid', None)
+        if uid is not None:
+            queryset = queryset.filter(uid=uid)
+        else:
+            queryset = Survey.objects.none()
+        return queryset
--- src/core/admin.py
+++ src/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from core.models import User
+from core.models import User, Survey
 
 
 admin.site.register(User)
+admin.site.register(Survey)
