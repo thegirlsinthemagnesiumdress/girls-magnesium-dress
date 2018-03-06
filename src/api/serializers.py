6ee24b3cacaf5e82957e39f0628356f184197075
Paolo <paolo@potatolondon.com>
2018-03-06 12:33:27 +0000

    Add dynamic properties to generate links for the survey [touch: 20]

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class SurveySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Survey
-        fields = ('company_name',)
+        fields = ('company_name', 'link', 'link_sponsor')
--- src/core/models.py
+++ src/core/models.py
@@ -8,12 +8,32 @@ import datetime
 class User(GaeAbstractDatastoreUser):
     pass
 
+SURVEY_URL = 'https://google.qualtrics.com/jfe/form/SV_beH0HTFtnk4A5rD'
+
 class Survey(models.Model):
-    uid = models.CharField(unique=True, editable=False, max_length=32)
     company_name= models.CharField(max_length=50)
+    uid = models.CharField(unique=True, editable=False, max_length=32)
 
     # Timestamps
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def link(self):
+        """
+        Lint to qualtrics survey. Every company will have a different URL, created
+        out of the uid.
+        The uid will be stored for every response and we will be able to used that
+        id to match the data against companies.
+        """
+        return '{}?sid={}'.format(SURVEY_URL, self.uid)
+
+    @property
+    def link_sponsor(self):
+        """
+        We need to differenziate between sponsor (survey creators)
+        amd participants. The way that is done is by setting sp=true.
+        """
+        return '{}&sp=true'.format(self.link)
 
     def save(self, *args, **kwargs):
         if self.pk is None:
