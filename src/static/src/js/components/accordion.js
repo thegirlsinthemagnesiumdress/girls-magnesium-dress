a7deea4f2ff84918d61dcafba877a1c392068159
Paolo <paolo@potatolondon.com>
2018-03-12 15:23:25 +0000

    Remove listeners on disconnect [touch: 21]

--- src/static/src/js/components/accordion.js
+++ src/static/src/js/components/accordion.js
@@ -11,17 +11,27 @@ const CLASSES = {
 export default class Accordion extends HTMLElement {
   constructor() {
     super();
+    this.toggle = this.toggle.bind(this);
   }
 
   connectedCallback () {
     this.$accordion = this.querySelector(DOM_SELECTORS.accordion);
     this.$accordionSections = this.querySelectorAll(DOM_SELECTORS.accordionSection);
     this.$accordionSectionHeadings = this.querySelectorAll(DOM_SELECTORS.accordionSectionHeadings);
+
     this.$accordionSectionHeadings.forEach((el) => {
-      el.addEventListener('click', this.toggle.bind(this));
+      el.addEventListener('click', this.toggle);
     });
+
     this.hideAll();
   }
+
+  disconnectedCallback () {
+    this.$accordionSectionHeadings.forEach((el) => {
+      el.removeEventListener('click', this.toggle);
+    });
+  }
+
 
   hideAll () {
     this.$accordionSections.forEach(function (el) {
