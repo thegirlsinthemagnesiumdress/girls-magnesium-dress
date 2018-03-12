c709df68627d01ed463e99d979f56979ec664245
Paolo <paolo@potatolondon.com>
2018-03-12 14:23:21 +0000

    Build slider section [touch: 31]

--- src/static/src/js/index.js
+++ src/static/src/js/index.js
@@ -1,9 +1,11 @@
 import fullpage from 'fullpage.js/dist/jquery.fullpage'; // eslint-disable-line no-unused-vars
 import jQuery from 'jquery';
+import 'slick-carousel';
 
-import ParallaxSection from './components/parallax-section'
-import Header from './components/header'
-import Registration from './components/registration'
+import ParallaxSection from './components/parallax-section';
+import Header from './components/header';
+import Registration from './components/registration';
+import SliderSection from './components/slider-section';
 import pubsub from './pubsub';
 
 window.$ = jQuery;
@@ -12,8 +14,10 @@ window.$ = jQuery;
 window.customElements.define('tr-parallax-section', ParallaxSection);
 window.customElements.define('tr-header', Header);
 window.customElements.define('tr-registration', Registration);
+window.customElements.define('tr-slider-section', SliderSection);
 
 window.$(document).ready(() => {
+
   window.$('#fullpage').fullpage({
     sectionSelector: '.fp-section',
     fixedElements: '#tr-header',
