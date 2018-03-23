92c35c5723def045377ddb0180760e56f88f9b72
Christopher Smith <christopher@potatolondon.com>
2018-03-23 14:47:28 +0000

    [touch: 20] Remove unnecessary test (model field default value is part of Django and is tested the Django project)

--- src/core/tests/test_models.py
+++ src/core/tests/test_models.py
@@ -1,17 +1,9 @@
 from djangae.test import TestCase
-from core.models import Survey, User
+from core.models import Survey
 from core.tests.mocks import generate_surveys
 
 import re
 
-class UserTest(TestCase):
-    def test_is_qualtrics_false_by_default(self):
-        user = User.objects.create(
-            username='test1',
-            email='test@test.com',
-            password='pass',
-        )
-        self.assertFalse(user.is_qualtrics)
 
 class SurveyTest(TestCase):
     def setUp(self):
@@ -35,7 +27,6 @@ class SurveyTest(TestCase):
         survey = self.surveys[0]
         match = re.search(r'sid=([^&]*)', survey.link)
         self.assertEqual(match.groups(1)[0], survey.uid)
-
 
     def test_sponsor_link(self):
         """
