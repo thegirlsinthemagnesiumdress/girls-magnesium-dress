e690b0d023304c3de6d5ef99f9de07a894d6a355
Christopher Smith <christopher@potatolondon.com>
2018-03-23 14:51:14 +0000

    [touch: 20] Whitespace and indentation fixes

--- src/core/tests/mocks.py
+++ src/core/tests/mocks.py
@@ -1,12 +1,13 @@
 from core.models import Survey
 
+
 def generate_surveys():
-  companies_name = ['1', '2', '3']
-  surveys = []
+    companies_name = ['1', '2', '3']
+    surveys = []
 
-  for company_name in companies_name:
-      s = Survey(company_name=company_name)
-      s.save()
-      surveys.append(s)
+    for company_name in companies_name:
+        s = Survey(company_name=company_name)
+        s.save()
+        surveys.append(s)
 
-  return surveys
+    return surveys
