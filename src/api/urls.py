59e2a4286ab101357a6d3a173ee550e7e65a1fa0
Christopher Smith <christopher@potatolondon.com>
2018-04-04 10:59:50 +0100

    [touch: 20] Rewrite company name API view to accept UID via GET parameter

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,5 +4,5 @@ from api.views import CreateSurveyView, SurveyCompanyNameFromUIDView
 
 urlpatterns = [
     url(r'^survey$', CreateSurveyView.as_view(), name='create_survey'),
-    url(r'^company-name/(?P<uid>\w{32})$', SurveyCompanyNameFromUIDView.as_view(), name='company_name'),
+    url(r'^company-name$', SurveyCompanyNameFromUIDView.as_view(), name='company_name'),
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, get_object_or_404
+from rest_framework.response import Response
 
 from api.serializers import SurveySerializer, SurveyCompanyNameSerializer
 from core.models import Survey
@@ -16,6 +17,7 @@ class CreateSurveyView(CreateAPIView):
 class SurveyCompanyNameFromUIDView(RetrieveAPIView):
     """
     External API endpoint to get the company name given a UID.
+    Must use `uid` GET param to specify company: e.g. ?uid=a95c7b47c8e2e1d057c56d114bb2862c
     """
     # Only allow authentication via token
     # Only using session authentication by default everywhere else
@@ -25,3 +27,11 @@ class SurveyCompanyNameFromUIDView(RetrieveAPIView):
     queryset = Survey.objects.all()
     lookup_field = 'uid'
     lookup_url_kwarg = 'uid'
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Overridden RetrieveAPIView retrieve() to get instance via GET param instead of URL keyword arg.
+        """
+        instance = get_object_or_404(self.queryset, **{self.lookup_field: request.GET.get(self.lookup_url_kwarg)})
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
