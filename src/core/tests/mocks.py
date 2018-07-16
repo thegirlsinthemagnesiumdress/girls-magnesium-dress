93e7858a1bf3b9423c741e9d57a876105dce51dc
Marco Azzalin <marco.azzalin@potatolondon.com>
2018-07-16 17:29:01 +0100

    [744472395780115] Remove unused code
    Change-Id: Id6698bf84d7b29c1b6fed8fb82144849bc80b6f3

--- src/core/tests/mocks.py
+++ src/core/tests/mocks.py
@@ -1,39 +1,5 @@
 from core.models import Survey
 
-weights = {
-    'Q3': 2,
-    'Q4': 0.5,
-    'Q7': 0.3,
-}
-
-response_0_questions = [
-    ('Q3', 2.0, 2, 'dimension_A'),
-    ('Q4', 0.0, 0.5, 'dimension_A'),
-    ('Q5_1', 2.0, 1, 'dimension_A'),
-    ('Q5_2', 0.0, 1, 'dimension_A'),
-    ('Q5_3', 3.0, 1, 'dimension_B'),
-    ('Q6', 4.0, 1, 'dimension_B'),
-    ('Q7', 2.0, 0.3, 'dimension_B'),
-    ('Q8', 4.0, 1, 'dimension_C'),
-    ('Q10', 0.0, 1, 'dimension_C'),
-    ('Q11', 1.0, 1, 'dimension_C'),
-    ('Q12', 2.0, 1, 'dimension_C'),
-]
-
-response_1_questions = [
-    ('Q3', 1.0, 2, 'dimension_A'),
-    ('Q4', 3.0, 0.5, 'dimension_A'),
-    ('Q5_1', 1.0, 1, 'dimension_A'),
-    ('Q5_2', 2.0, 1, 'dimension_A'),
-    ('Q5_3', 4.0, 1, 'dimension_B'),
-    ('Q6', 0.0, 1, 'dimension_B'),
-    ('Q7', 1.0, 0.3, 'dimension_B'),
-    ('Q8', 0.0, 1, 'dimension_C'),
-    ('Q10', 0.0, 1, 'dimension_C'),
-    ('Q11', 1.0, 1, 'dimension_C'),
-    ('Q12', 0.0, 1, 'dimension_C'),
-]
-
 
 qualtrics_export = {
     'responses': [
@@ -128,29 +94,6 @@ qualtrics_export = {
             'Q12': '4',
         }
     ]
-}
-
-dimensions = {
-    'dimension_A': ['Q3', 'Q4', 'Q5_1', 'Q5_2'],
-    'dimension_B': ['Q5_3', 'Q6', 'Q7'],
-    'dimension_C': ['Q8', 'Q10', 'Q11', 'Q12'],
-}
-
-response_1_overall = 1.907407407
-
-DMB = {
-    'survey': {
-        '1': {
-            'overall': 1.5,
-            'best_practice': 0,
-            'category_overall': {
-                'dimension_A': 1.388888889,  # 1.125,
-            },
-            'category_best_practice': {
-                'dimension_A': 0,  # 1.75,
-            },
-        }
-    }
 }
 
 
