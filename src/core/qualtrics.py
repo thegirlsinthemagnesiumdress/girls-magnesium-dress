fa168df9a4e9a5248ec6761f7617f4e014865146
Marco Azzalin <marco.azzalin@potatolondon.com>
2018-07-16 17:32:21 +0100

    [744472395780115] Replace dict with defaultdict
    Change-Id: I6ed81a3f46056fbca93312ec9516e6f4fc9cb7a7

--- src/core/qualtrics.py
+++ src/core/qualtrics.py
@@ -1,5 +1,6 @@
 import numpy
 from django.conf import settings
+from collections import defaultdict
 
 
 def _weighted_questions_average(questions_array):
@@ -11,36 +12,32 @@ def _weighted_questions_average(questions_array):
     Returns:
         float -- weighted average.
     """
-
     values = map(lambda x: x[1], questions_array)
     weights = map(lambda x: x[2], questions_array)
 
     return numpy.average(values, weights=weights)
 
 
-def calculate_group_benchmark(filtered_responses, by_dimension=False):
+def calculate_group_benchmark(filtered_responses):
     """ Calculates benchmark on the responses filtered dataset
 
     Arguments:
         questions {list} -- Array of responses
 
     Keyword Arguments:
-        by_dimension {bool} -- Whether or not return a dict with benchmark by dimension (default: {False})
+        filtered_responses {array} -- Array of tuple (q_id, q_value, q_weight)
     """
+    # each element of the dictionary will be key: list of weighted average by dimension
+    responses_benchmarks_by_dimension = defaultdict(list)
 
     # Loop on a every single response.
-    responses_benchmarks_by_dimension = {}
-
     for response in filtered_responses:
         # Create a dict where single response benchmarks aggregated by dimension.
         _, response_benchmarks_by_dimension = calculate_response_benchmark(response)
 
         for dimension in settings.DIMENSIONS:
             benchmark = response_benchmarks_by_dimension.get(dimension, 0)
-            if responses_benchmarks_by_dimension.get(dimension):
-                responses_benchmarks_by_dimension[dimension].append(benchmark)
-            else:
-                responses_benchmarks_by_dimension[dimension] = [benchmark]
+            responses_benchmarks_by_dimension[dimension].append(benchmark)
 
     benchmark_by_dimension = {}
 
@@ -54,13 +51,10 @@ def calculate_group_benchmark(filtered_responses, by_dimension=False):
 def calculate_response_benchmark(response_questions):
 
     # Create a dict where single response questions are aggregated by dimension.
-    questions_by_dimension = {}
+    questions_by_dimension = defaultdict(list)
+
     for question in response_questions:
-        dimension = question[3]
-        if questions_by_dimension.get(dimension):
-            questions_by_dimension[dimension].append(question)
-        else:
-            questions_by_dimension[dimension] = [question]
+        questions_by_dimension[question[3]].append(question)
 
     # Create a dict where single response benchmarks aggregated by dimension.
     benchmark_by_dimension = {}
