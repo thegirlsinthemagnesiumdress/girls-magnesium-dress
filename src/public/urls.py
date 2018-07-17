fe77947969b501194c26f9783d11af4499b313d1
Paolo <pchillari@google.com>
2018-07-17 14:11:20 +0100

    Work on reports views [touch: 744472395780115]
    Change-Id: I52a4ed68165b0f2705ebcf6c95792d2e09268d0a

--- src/public/urls.py
+++ src/public/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from public.views import report_view, reports_list
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="public/index.html"), name="index"),
     url(r'^registration$', TemplateView.as_view(template_name="public/registration.html"), name="registration"),
-    url(r'^reports/(?P<sid>[\w]+)$', TemplateView.as_view(template_name="public/report.html"), name="report"),
+    url(r'^reports$', reports_list, name="reports"),
+    url(r'^reports/(?P<sid>[\w]+)$', report_view, name="report"),
 ]
--- src/public/views.py
+++ src/public/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
 from django.http import Http404
-from core.models import SurveyResult
-from core.qualtrics import calculate_benchmark
+from core.models import SurveyResult, Survey
+from core.qualtrics import calculate_response_benchmark, calculate_group_benchmark
 
 
-def ReportsView(request, sid):
+def reports_list(request):
+    s_results = SurveyResult.objects.all()
+    surveys = []
+
+    for result in s_results:
+        if result.survey:
+            surveys.append(result.survey)
+
+    return render(request, 'public/reports-list.html', {
+        'surveys': set(surveys),
+    })
+
+
+def report_view(request, sid):
     try:
-        sResult = SurveyResult.objects.get(survey__sid=sid)
+        s = Survey.objects.filter(sid=sid).first()
+        s_result = SurveyResult.objects.filter(survey=s).latest('loaded_at')
+    except Survey.DoesNotExist:
+        raise Http404("Survey does not exist.")
     except SurveyResult.DoesNotExist:
-        raise Http404("Report has not been generated yet")
+        raise Http404("Report has not been generated yet.")
 
-    DMB, DMBd = calculate_benchmark(sResult.questions)
-
+    DMB, DMBd = calculate_response_benchmark(s_result.questions)
     return render(request, 'public/report.html', {
-        DMB,
-        DMBd
+        'company_name': s.company_name,
+        'DMB': DMB,
+        'DMBd': DMBd,
     })
