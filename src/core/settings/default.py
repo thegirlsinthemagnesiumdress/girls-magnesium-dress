1fcfdf5e2edc37a21219f640c33313ec182ab64c
Paolo <pchillari@google.com>
2018-07-19 15:59:01 +0100

    CR feedback [touch: 749842967267220]
    Change-Id: I079079e98469f90ae70247a6fbcf6cb8a445b75f

--- src/core/settings/default.py
+++ src/core/settings/default.py
@@ -264,5 +264,6 @@ AUTHENTICATION_BACKENDS = (
 )
 
 RESPONSE_EXPORT_BASE_URL = 'https://{0}.qualtrics.com/API/v3/responseexports/'.format('google.co1')
+QUALTRICS_REQUEST_DEADLINE = 60
 
 from .constants import *
