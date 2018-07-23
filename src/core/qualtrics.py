1c811a97760dc1a4178a4ec9fd158bc207a4b911
Marco Azzalin <marco.azzalin@potatolondon.com>
2018-07-23 13:56:19 +0100

    [touch:752953467161281]Move download_results in qualtrics module
    Change-Id: Ifc9dcd3bd9877716b9048b27ac5ca2b4b2bbeabf

--- src/core/qualtrics.py
+++ src/core/qualtrics.py
@@ -1,6 +1,12 @@
+import io
+import json
+import zipfile
+from collections import defaultdict
+
+from google.appengine.api import urlfetch
+
 import numpy
 from django.conf import settings
-from collections import defaultdict
 
 
 def _weighted_questions_average(questions_array):
@@ -62,3 +68,72 @@ def calculate_response_benchmark(response_questions):
         benchmark_by_dimension[dimension] = _weighted_questions_average(questions)
 
     return numpy.average(benchmark_by_dimension.values()), benchmark_by_dimension
+
+
+def download_results(response_id=None, file_format='json'):
+    progress_status = 'in progress'
+    request_check_progress = 0
+    qualtrics_data = {}
+    headers = {
+        'content-type': 'application/json',
+        'x-api-token': settings.QUALTRICS_API_TOKEN,
+    }
+
+    # Step 1: Creating Data Export
+    data_export_payload = {
+        'format': file_format,
+        'surveyId': settings.QUALTRICS_SURVEY_ID,
+    }
+
+    if response_id:
+        data_export_payload['lastResponseId'] = response_id
+
+    download_request_response = urlfetch.fetch(
+        method=urlfetch.POST,
+        url=settings.RESPONSE_EXPORT_BASE_URL,
+        deadline=settings.QUALTRICS_REQUEST_DEADLINE,
+        payload=json.dumps(data_export_payload),
+        headers=headers
+    )
+
+    progress_id = json.loads(download_request_response.content)['result']['id']
+
+    # Step 2: Checking on Data Export Progress and waiting until export is ready
+    while request_check_progress < 100 and progress_status is not 'complete':
+        request_check_url = ''.join((settings.RESPONSE_EXPORT_BASE_URL, progress_id))
+        request_check_response = urlfetch.fetch(
+            method=urlfetch.GET,
+            url=request_check_url,
+            deadline=settings.QUALTRICS_REQUEST_DEADLINE,
+            headers=headers)
+        request_check_progress = json.loads(request_check_response.content)['result']['percentComplete']
+
+    # Step 3: Downloading file
+    request_download_url = ''.join((settings.RESPONSE_EXPORT_BASE_URL, progress_id, '/file'))
+
+    request_download = urlfetch.fetch(
+        method='GET',
+        url=request_download_url,
+        headers=headers,
+        deadline=settings.QUALTRICS_REQUEST_DEADLINE
+    )
+
+    # Step 4: Unziping file
+    with io.BytesIO() as in_memory_buffer:
+        in_memory_buffer.write(request_download.content)
+
+        # it seems slightly overengineered to write `_unpack_zip` function to loop through a zip
+        # file that should most likely cointain only one file (it will cointain more than one
+        # file in case the number of responses is abouve 1M), however, looping through the zip
+        # archive allow us to read file content anonymously.
+        qualtrics_data = [response for response in _unpack_zip(in_memory_buffer)]
+        qualtrics_data = qualtrics_data[0]
+
+    return json.loads(qualtrics_data)
+
+
+def _unpack_zip(in_memory_buffer):
+    with zipfile.ZipFile(in_memory_buffer) as thezip:
+        for zipinfo in thezip.infolist():
+            with thezip.open(zipinfo) as thefile:
+                yield thefile.read()
--- src/core/tasks.py
+++ src/core/tasks.py
@@ -1,11 +1,7 @@
-import io
-import json
-import zipfile
-
-from core.models import Survey, SurveyResult
-from django.conf import settings
 import logging
-from google.appengine.api import urlfetch
+
+import qualtrics
+from core.models import Survey, SurveyResult
 
 
 def get_results():
@@ -17,7 +13,7 @@ def get_results():
         response_id = None
         logging.info('No Survey results has already been downloaded so far, download all the results.')
 
-    results = download_results(response_id=response_id)
+    results = qualtrics.download_results(response_id=response_id)
     _create_survey_result(results.get('responses'))
 
 
@@ -35,72 +31,3 @@ def _create_survey_result(results_data):
             response_id=data.get('ResponseID'),
             data=data,
         )
-
-
-def download_results(response_id=None, file_format='json'):
-    progress_status = 'in progress'
-    request_check_progress = 0
-    qualtrics_data = {}
-    headers = {
-        'content-type': 'application/json',
-        'x-api-token': settings.QUALTRICS_API_TOKEN,
-    }
-
-    # Step 1: Creating Data Export
-    data_export_payload = {
-        'format': file_format,
-        'surveyId': settings.QUALTRICS_SURVEY_ID,
-    }
-
-    if response_id:
-        data_export_payload['lastResponseId'] = response_id
-
-    download_request_response = urlfetch.fetch(
-        method=urlfetch.POST,
-        url=settings.RESPONSE_EXPORT_BASE_URL,
-        deadline=settings.QUALTRICS_REQUEST_DEADLINE,
-        payload=json.dumps(data_export_payload),
-        headers=headers
-    )
-
-    progress_id = json.loads(download_request_response.content)['result']['id']
-
-    # Step 2: Checking on Data Export Progress and waiting until export is ready
-    while request_check_progress < 100 and progress_status is not 'complete':
-        request_check_url = ''.join((settings.RESPONSE_EXPORT_BASE_URL, progress_id))
-        request_check_response = urlfetch.fetch(
-            method=urlfetch.GET,
-            url=request_check_url,
-            deadline=settings.QUALTRICS_REQUEST_DEADLINE,
-            headers=headers)
-        request_check_progress = json.loads(request_check_response.content)['result']['percentComplete']
-
-    # Step 3: Downloading file
-    request_download_url = ''.join((settings.RESPONSE_EXPORT_BASE_URL, progress_id, '/file'))
-
-    request_download = urlfetch.fetch(
-        method='GET',
-        url=request_download_url,
-        headers=headers,
-        deadline=settings.QUALTRICS_REQUEST_DEADLINE
-    )
-
-    # Step 4: Unziping file
-    with io.BytesIO() as in_memory_buffer:
-        in_memory_buffer.write(request_download.content)
-
-        # it seems slightly overengineered to write `_unpack_zip` function to loop through a zip
-        # file that should most likely cointain only one file (it will cointain more than one
-        # file in case the number of responses is abouve 1M), however, looping through the zip
-        # archive allow us to read file content anonymously.
-        qualtrics_data = [response for response in _unpack_zip(in_memory_buffer)]
-        qualtrics_data = qualtrics_data[0]
-
-    return json.loads(qualtrics_data)
-
-
-def _unpack_zip(in_memory_buffer):
-    with zipfile.ZipFile(in_memory_buffer) as thezip:
-        for zipinfo in thezip.infolist():
-            with thezip.open(zipinfo) as thefile:
-                yield thefile.read()
