ebb2763871cb3ccfd4817c0684728009966fa68e
Paolo <pchillari@google.com>
2018-07-24 18:45:45 +0100

    Update view to use new saved benchmarks [touch: 752953467161281]
    Change-Id: Id9388b39d43f65b77a81e479e77b86d0154ec360

--- src/public/views.py
+++ src/public/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
 from core.models import SurveyResult, Survey
-from core.qualtrics.benchmark import calculate_response_benchmark
 
 
 def reports_list(request):
@@ -26,9 +25,8 @@ def report_view(request, sid):
     except SurveyResult.DoesNotExist:
         raise Http404("Report has not been generated yet.")
 
-    DMB, DMBd = calculate_response_benchmark(s_result.questions)
     return render(request, 'public/report.html', {
         'company_name': s.company_name,
-        'DMB': DMB,
-        'DMBd': DMBd,
+        'DMB': s_result.dmb,
+        'DMBd': s_result.dmb_d,
     })
