54e58bbeb321c1545425a780a9557806d00a6ca2
Paolo <pchillari@google.com>
2018-07-30 15:17:14 +0100

    Add test header to test styles and scripts [touch: 756613374141374]
    Change-Id: I154ee9456cdfd68a37bc9be4c3d47f3bbbb3ddf6

--- src/static/src/js/app.js
+++ src/static/src/js/app.js
@@ -4,11 +4,14 @@ goog.module('dmb.app');
 const glueApp = goog.require('glue.app');
 const glueCommon = goog.require('glue.ng.common');
 const glueZippy = goog.require('glue.ng.zippy');
+const header = goog.require('glue.ng.ui.header');
 
 /** @type {!angular.Module} */
 const module = angular.module('dmb', [
   glueCommon.module.name, // Progressive enhancement/browser detections.
   glueZippy.module.name,
+  'ngAnimate',
+  header.module.name,
   'hercules_template_bundle'
 ]);
 
