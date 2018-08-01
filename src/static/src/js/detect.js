7f0d1179240c43a155a81aeec28b1ea17bad900e
Paolo <pchillari@google.com>
2018-08-01 10:44:08 +0100

    Make sure we decorate the dom with flexbox class [touch: 762817590471872]
    Change-Id: I918dfb42fc5f83bfc6f946a7871fdb44106f8f39

--- src/static/src/js/detect.js
+++ src/static/src/js/detect.js
@@ -3,7 +3,14 @@
  * differently and prevent FOUC.
  */
 goog.module('dmb.detect');
+
+
 const glueApp = goog.require('glue.app');
+const glueDetect = goog.require('glue.detect');
+const glueFlexbox = goog.require('glue.detect.flexbox');
+
+glueDetect.decorateDom(glueFlexbox);
+
 glueApp.blacklist({
   // Set this to the highest level of IE you don't support.
   // Glue 16+ doesn't support IE 10 or lower.
