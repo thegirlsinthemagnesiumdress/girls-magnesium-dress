d38492be8204b9a5c494aba9eb4622ba46f6aa4c
Marco Azzalin <marco.azzalin@potatolondon.com>
2018-08-03 11:56:47 +0100

    Removed unused view
    Change-Id: I93e068abd9afa185ddaaac3a089c80f53474126e

--- src/public/urls.py
+++ src/public/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from public.views import report_view, reports_list, reports_admin
+from public.views import report_view, reports_admin
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="public/index.html"), name="index"),
     url(r'^registration$', TemplateView.as_view(template_name="public/registration.html"), name="registration"),
-    url(r'^reports$', reports_list, name="reports"),
+    url(r'^reports$', reports_admin, name="reports"),
     url(r'^reports/(?P<sid>[\w]+)$', report_view, name="report"),
-    url(r'^reports_admin$', reports_admin, name="reports_admin"),
 ]
--- src/public/views.py
+++ src/public/views.py
@@ -4,30 +4,14 @@ from django.http import Http404
 from django.shortcuts import render
 
 
-def reports_list(request):
-    s_results = SurveyResult.objects.all()
-    surveys = []
-
-    for result in s_results:
-        if result.survey:
-            surveys.append(result.survey)
-
-    return render(request, 'public/reports-list.html', {
-        'surveys': set(surveys),
-    })
-
-
 def report_view(request, sid):
     try:
-        s = Survey.objects.get(sid=sid)
-        s_result = SurveyResult.objects.filter(survey=s).latest('loaded_at')
-    except Survey.DoesNotExist:
-        raise Http404("Survey does not exist.")
+        s_result = SurveyResult.objects.filter(survey_id=sid).latest('loaded_at')
     except SurveyResult.DoesNotExist:
-        raise Http404("Report has not been generated yet.")
+        raise Http404("Report does not exist.")
 
     return render(request, 'public/report.html', {
-        'company_name': s.company_name,
+        'company_name': s_result.survey.company_name,
         'DMB': s_result.dmb,
         'DMBd': s_result.dmb_d,
     })
@@ -37,13 +21,20 @@ def report_view(request, sid):
 def reports_admin(request):
 
     if request.user.is_whitelisted:
-        s_results = SurveyResult.objects.exclude(survey__isnull=True)
+        s_results = SurveyResult.objects.all()
     else:
         engagement_lead_surveys = Survey.objects.filter(engagement_lead=request.user.engagement_lead)
         s_results = SurveyResult.objects.filter(survey__in=engagement_lead_surveys)
 
-    surveys = [result.survey for result in s_results if result.survey]
+    surveys_with_results = []
+    surveys_without_results = []
+
+    for result in s_results:
+        if result.survey:
+            surveys_with_results.append(result.survey)
+        else:
+            surveys_without_results.append(result)
 
     return render(request, 'public/reports-list.html', {
-        'surveys': surveys,
+        'surveys': surveys_with_results,
     })
