f8d4b399dc4e99de07b9bd96ccf68453336010d6
Michael Strutt <mstrutt@potatolondon.com>
2018-08-21 14:15:59 +0100

    Fixing icon location, include example HTML
    Change-Id: Id29a06fd3685136d60b0dbe6f039620821904d7a

--- src/static/src/js/components/side-panel/side-panel.directive.js
+++ src/static/src/js/components/side-panel/side-panel.directive.js
@@ -27,3 +27,27 @@ exports = {
   main: SidePanelDirective,
   DIRECTIVE_NAME: SidePanelDirective.DIRECTIVE_NAME,
 };
+
+/*
+
+  EXAMPLE HTML:
+
+  <div id="some-id" class="dmb-side-panel dmb-side-panel--narrow" dmb-side-panel>
+
+    <button class="dmb-side-panel__fab dmb-fab" dmb-side-panel-close>
+      <span class="dmb-fab__icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <use xlink:href="#mi-close"></use>
+        </svg>
+      </span>
+      <span class="dmb-fab__text">Close</span>
+    </button>
+
+    <div class="dmb-side-panel__inner">
+      <h1 class="h-c-headline h-c-headline--two dmb-h-mb--medium">Side panel title</h1>
+      [Side panel content goes here…]
+    </div>
+
+  </div>
+
+*/
