f62f4654e469bf81a519bf3396ab42c6a8027d9d
Michael Strutt <mstrutt@potatolondon.com>
2018-08-24 11:49:19 +0100

    Finishing up tweaks to the header, adding in the logo
    Change-Id: Ia4edc163cded83004458db591199323bce4c0fae

--- src/static/src/js/survey.js
+++ src/static/src/js/survey.js
@@ -13,10 +13,12 @@ goog.module('dmb.survey');
  */
 window.Qualtrics;
 
+const focusControl = goog.require('dmb.components.focusControl.run');
 const scrollPosition = goog.require('dmb.survey.scrollPosition');
 const sidePanel = goog.require('dmb.survey.sidePanel');
 const stickyProgress = goog.require('dmb.survey.stickyProgress');
 
+focusControl.main();
 scrollPosition.init();
 sidePanel.init();
 stickyProgress.init();
