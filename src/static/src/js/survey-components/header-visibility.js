6dedd0c5bc7e3e2ba71e1f97290c304df030818f
Michael Strutt <mstrutt@potatolondon.com>
2018-08-31 12:19:05 +0100

    Updates to the survey styling
    Change-Id: I219b1a9e2cc10828041f8bdb3515989eefbaab1b

---
+++ src/static/src/js/survey-components/header-visibility.js
@@ -0,0 +1,63 @@
+goog.module('dmb.survey.headerVisibility');
+
+/**
+ * Binds the event listener
+ */
+function init() {
+  /*
+   * Possible events:
+   * - addOnload
+   * - addOnReady
+   * - addOnUnload
+   */
+  window['Qualtrics']['SurveyEngine']['addOnload'](tempHideHeader);
+  window['Qualtrics']['SurveyEngine']['addOnReady'](setHeaderVisibility);
+}
+
+/**
+ * Sets the scroll position to the first error or the top of the questions (if
+ * not on the first page or the survey). Depends on the progress bar for
+ * working this out.
+ */
+function setHeaderVisibility() {
+  const progress = document.getElementById('ProgressBarFillContainer');
+  const header = document.getElementById('LogoContainer');
+  const hiddenClass = 'dmb-h-hidden';
+
+  if (!progress || !header) {
+    return;
+  }
+
+  const progressText = progress.textContent.match(/[.\d]+%/);
+  const progressPercent = progressText && progressText[0];
+
+  if (!progressPercent || progressPercent === '0%') {
+    header.classList.remove(hiddenClass);
+    return;
+  }
+
+  header.classList.add(hiddenClass);
+}
+
+
+/**
+ * the onload event happens before the progress is updated, and the onReady
+ * event happens after they've scrolled up to show the header. This hides the
+ * header temporarily for that transitional period until we can properly
+ * detect if it should be there
+ */
+function tempHideHeader() {
+  const header = document.getElementById('LogoContainer');
+  const hiddenClass = 'dmb-h-hidden';
+
+  if (!header) {
+    return;
+  }
+
+  header.classList.add(hiddenClass);
+}
+
+
+exports = {
+  init,
+};
