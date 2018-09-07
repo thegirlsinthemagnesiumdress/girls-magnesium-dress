89771b910a36e35a3393b53f5daf8953a66b6fcf
Marco Azzalin <marco.azzalin@potatolondon.com>
2018-09-07 15:28:05 +0200

    [touch:769218264107747]Update middleware to redirect to login id needed.
    Change-Id: I7c90bce75419af897932e9670371e1f507d3a5b0

--- src/core/tests/test_auth.py
+++ src/core/tests/test_auth.py
@@ -39,7 +39,7 @@ class DomainRestrictionMiddlewareTests(TestCase):
         User.objects.create(email='test@example.com')
 
     @with_appengine_user(email='test@gmail.com')
-    @override_settings(ALLOWED_AUTH_DOMAINS=["google.com"])
+    @override_settings(ALLOWED_AUTH_DOMAINS=["google.com"], DJANGAE_CREATE_UNKNOWN_USER=True)
     def test_users_restricted_by_domain_not_allowed(self):
         """When a user is not in ALLOWED_AUTH_DOMAINS returns 403."""
         response = self.client.get("/")
@@ -62,16 +62,16 @@ class DomainRestrictionMiddlewareTests(TestCase):
     @with_appengine_anon
     @override_settings(ALLOWED_AUTH_DOMAINS=["google.com"])
     def test_domain_restriction_not_allows_anonymous_users(self):
-        """Anonymous users are not accepted. returns 403."""
+        """Anonymous users are not accepted. It should redirect to login 302."""
         response = self.client.get("/")
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 302)
 
     @with_appengine_anon
     @override_settings(ALLOWED_AUTH_DOMAINS=["*"])
     def test_domain_star_not_allows_anonymous_users(self):
-        """Anonymous users are not accepted, even with domain *. returns 403."""
+        """Anonymous users are not accepted, even with domain *. Redirects to login 302."""
         response = self.client.get("/")
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 302)
 
     @with_appengine_user(email='test@example.com')
     @override_settings(ALLOWED_AUTH_DOMAINS=["*"])
@@ -81,24 +81,41 @@ class DomainRestrictionMiddlewareTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     @with_appengine_user(email='test@gmail.com')
-    @override_settings(ALLOWED_AUTH_DOMAINS=["*"])
-    def test_domain_star_only_allows_registered_users(self):
+    @override_settings(ALLOWED_AUTH_DOMAINS=["*"], DJANGAE_CREATE_UNKNOWN_USER=False)
+    def test_domain_star_only_allows_registered_users_when_create_unknown_false(self):
         """
-        When ALLOWED_AUTH_DOMAINS is *, any registered user is accepted,
-        but test@gmail.com is not, so returns 403.
+        When ALLOWED_AUTH_DOMAINS is *, and DJANGAE_CREATE_UNKNOWN_USER is `False`
+        any registered user is accepted, but test@gmail.com is not, because djangae
+        prevents to create users other then the existing ones. So it redirects back
+        to login, redirects 302.
         """
         response = self.client.get("/")
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 302)
+
+    @with_appengine_user(email='test@gmail.com')
+    @override_settings(ALLOWED_AUTH_DOMAINS=["*"], DJANGAE_CREATE_UNKNOWN_USER=True)
+    def test_domain_star_only_allows_registered_users_when_create_unknown_true(self):
+        """
+        When ALLOWED_AUTH_DOMAINS is *, and DJANGAE_CREATE_UNKNOWN_USER is `True`,
+        any logged user is accepted, even test@gmail.com, bacause djange can create
+        unknown users, it returns 200.
+        """
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
 
     @with_appengine_anon
-    @override_settings(ALLOWED_AUTH_DOMAINS=["*"])
-    def test_login_required_page_anon_user_access_denied(self):
-        """
-        A login required page for anonymous user, even when ALLOWED_AUTH_DOMAINS is *,
-        the access is denied. Returns 403.
-        """
+    @override_settings(ALLOWED_AUTH_DOMAINS=["*"], DJANGAE_CREATE_UNKNOWN_USER=False)
+    def test_login_required_page_anon_user_access_denied_when_create_unknown_false(self):
+        """A login required page for anonymous it will rediret to login. Returns 302."""
         response = self.client.get("/logged_in_view/")
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 302)
+
+    @with_appengine_anon
+    @override_settings(ALLOWED_AUTH_DOMAINS=["*"], DJANGAE_CREATE_UNKNOWN_USER=True)
+    def test_login_required_page_anon_user_access_denied_when_create_unknown_true(self):
+        """A login required page for anonymous it will rediret to login. Returns 302."""
+        response = self.client.get("/logged_in_view/")
+        self.assertEqual(response.status_code, 302)
 
     @with_appengine_user(email='test@example.com')
     @override_settings(ALLOWED_AUTH_DOMAINS=["*"])
