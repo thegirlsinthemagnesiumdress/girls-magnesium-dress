92a15eb58f71deda59d7966f5c582e03d1fc5bde
Paolo <paolo@potatolondon.com>
2018-09-07 15:07:50 +0100

    Remove toolbar from staging [touch: 769218264107747]
    Change-Id: I3965e2569db04bfed9bade7ebc386b43d40de806

--- src/core/settings/staging.py
+++ src/core/settings/staging.py
@@ -1,10 +1,6 @@
 from core.settings.default import *  # noqa
 
 
-INSTALLED_APPS = tuple(list(INSTALLED_APPS) + [
-    'debug_toolbar',
-])
-
 MIDDLEWARE_CLASSES = tuple(
     list(MIDDLEWARE_CLASSES) +
     [
