e57565f5957dd46dc92732ab1fe77c6e3c43b7ad
Paolo <paolo@potatolondon.com>
2018-09-12 12:23:37 +0100

    Update url from survey creation page [touch: 809773758954666]
    Change-Id: Ibb5cd0d94651c2d8b068572df52c06cbe1e24bd7

--- src/public/urls.py
+++ src/public/urls.py
@@ -5,7 +5,7 @@ from public.views import report_view, reports_admin, registration
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="public/index.html"), name="index"),
-    url(r'^registration$', registration, name="registration"),
+    url(r'^createsurvey$', registration, name="registration"),
     url(r'^reports$', reports_admin, name="reports"),
     url(r'^reports/(?P<sid>[\w]+)$', report_view, name="report"),
     # Don't merge with this, it's just for testing
