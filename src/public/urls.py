932c0f4f30e69bcf9e5eac7d19af15c83a536974
Paolo <paolo@potatolondon.com>
2018-09-28 12:52:53 +0100

    Urls trailing slash [touch:837223384838730]
    Change-Id: Id6fbf04dd42476624a4ce10c31d5e4a5ddb8f031

--- src/public/urls.py
+++ src/public/urls.py
@@ -5,7 +5,7 @@ from public.views import reports_admin, registration
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="public/index.html"), name="index"),
-    url(r'^createsurvey$', registration, name="registration"),
-    url(r'^reports$', reports_admin, name="reports"),
-    url(r'^reports/(?P<sid>[\w]+)$', TemplateView.as_view(template_name="public/report-static.html"), name="report"),
+    url(r'^createsurvey/$', registration, name="registration"),
+    url(r'^reports/$', reports_admin, name="reports"),
+    url(r'^reports/(?P<sid>[\w]+)/$', TemplateView.as_view(template_name="public/report-static.html"), name="report"),
 ]
