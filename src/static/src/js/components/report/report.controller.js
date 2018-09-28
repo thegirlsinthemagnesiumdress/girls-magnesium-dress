f7b165a6822aa1b3188f63464ed04e106b7670a3
Paolo <paolo@potatolondon.com>
2018-09-28 13:06:31 +0100

    Fix sid regex [touch: 837223384838730]
    Change-Id: I34bc67bbea144af47115b202adff03c1cffe9efa

--- src/static/src/js/components/report/report.controller.js
+++ src/static/src/js/components/report/report.controller.js
@@ -2,7 +2,7 @@ goog.module('dmb.components.report.controller');
 
 const surveyEndpoint = '/api/report/company/';
 const industryEndpoint = '/api/report/industry/';
-const locationSidRegex = /\/(\w+)[^\/\!\?#]?[^\/]*$/;
+const locationSidRegex = /reports\/(\w+)[#\/].*$/;
 
 const PaginationModel = goog.require('glue.ng.pagination.Model');
 
