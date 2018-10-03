54603bbfca72421dd1e1c09fc390733555e94e07
Paolo <paolo@potatolondon.com>
2018-10-03 15:03:38 +0100

    SUPER_USER to SUPER_USERS [touch: 811998878819681]
    Change-Id: I2511831d8a96b2472e17e3c792c5e19b4e2c8901

--- src/core/settings/default.py
+++ src/core/settings/default.py
@@ -291,7 +291,7 @@ QUALTRICS_SURVEY_ID = 'SV_beH0HTFtnk4A5rD'
 QUALTRICS_EMAIL_TO = 'Q97_4_TEXT'
 QUALTRICS_EMAIL_BCC = 'Q97_5_TEXT'
 
-SUPER_USER = [
+SUPER_USERS = [
     'alfredb@google.com',
     'patricks@google.com',
     'bbelcastro@google.com',
