0938ad5ffaa2fb2699f2dcf0db35b8b0f20f70e0
Paolo <paolo@potatolondon.com>
2018-11-29 16:33:09 +0000

    HOTFIX- Fix annotation typo
    Change-Id: Ic0d973b3ce13e18c9744c9c6e0580376146168c8

--- src/static/src/js/components/force-reflow/force-reflow.util.js
+++ src/static/src/js/components/force-reflow/force-reflow.util.js
@@ -2,7 +2,7 @@ goog.module('dmb.components.forceReflow.util');
 
 /**
  * It forces a browser reflow.
- * @param {HTNLElement} element
+ * @param {HTMLElement} element
  */
 function forceReflow(element=document.body) {
   window['__forceReflow__'] = !!element.offsetHeight;
