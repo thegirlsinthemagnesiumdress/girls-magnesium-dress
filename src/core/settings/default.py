834e8b3c861bd04dff1d47cc914b260a9911b892
Paolo <paolo@potatolondon.com>
2018-12-03 10:56:28 +0000

    HOTFIX - Add cron task handler to be admin only
    Change-Id: I148117d7ac5b875645f008cbcc7c45e8c1d6471b

--- src/core/settings/default.py
+++ src/core/settings/default.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = (
     'archivable',
     'gulpify',
-    'djangae', # Djangae should be before Django core/contrib things
+    'djangae',  # Djangae should be before Django core/contrib things
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
