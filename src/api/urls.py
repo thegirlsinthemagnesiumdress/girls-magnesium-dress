62988799905f8e87c8b06470987bed5b4b0f4dbd
Marco Azzalin <marco.azzalin@potatolondon.com>
2019-01-31 15:46:23 +0000

    [touch:1107554847434775] Change url to less measleading one

--- src/api/urls.py
+++ src/api/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     url(r'^survey$', CreateSurveyView.as_view(), name='create_survey'),
     url(r'^company-name$', SurveyCompanyNameFromUIDView.as_view(), name='company_name'),
     url(r'^report/company/(?P<sid>[0-9a-f]{32})/$', SurveyDetailView.as_view(), name='survey_report'),
-    url(r'^report/(?P<response_id>\w+)/$', SurveyResultDetailView.as_view(), name='survey_result_report'),
+    url(r'^report/result/(?P<response_id>\w+)/$', SurveyResultDetailView.as_view(), name='survey_result_report'),
     url(r'^report/industry/(?P<industry>[\w-]+)/$', SurveyResultsIndustryDetail.as_view(), name='survey_industry'),
 ]
