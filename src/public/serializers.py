b4064a85322f83c70e53ff6aacf5ae10644446bd
Paolo <paolo@potatolondon.com>
2019-02-01 13:23:19 +0000

    HOTFIX - Remove unusused count from serializer
    Change-Id: I374306e7e529b96495ccc60945dba1a58dbf27af

--- src/public/serializers.py
+++ src/public/serializers.py
@@ -13,10 +13,6 @@ class AdminSurveyResultsSerializer(ModelSerializer):
     last_survey_result = AdminSurveyResultSerializer(read_only=True)
     country_name = CharField(source='get_country_display')
     industry_name = CharField(source='get_industry_display')
-    survey_results_count = IntegerField(
-        source='survey_results.count',
-        read_only=True
-    )
     last_survey_result_link = CharField(read_only=True)
     survey_results = AdminSurveyResultSerializer(many=True, read_only=True)
 
@@ -29,7 +25,6 @@ class AdminSurveyResultsSerializer(ModelSerializer):
             'country_name',
             'last_survey_result',
             'created_at',
-            'survey_results_count',
             'last_survey_result_link',
             'survey_results',
             'link_sponsor',
