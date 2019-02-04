61fbe1906eb97d65dd5810ece677f0a632d2cadd
Paolo <paolo@potatolondon.com>
2019-02-04 18:25:25 +0000

    HOTFIX - Ordering by date in admin page
    Change-Id: Id96b50a19b21ebd311d183e12e59470730aa9fc2

--- src/static/src/js/tablesort-utility.js
+++ src/static/src/js/tablesort-utility.js
@@ -2,8 +2,8 @@ goog.module('dmb.tableCustomSort');
 
 const configureAdditionalSortTypes = ['glueTableSortServiceProvider', function(glueTableSortServiceProvider) {
   glueTableSortServiceProvider.setSortType('datetime-custom', function(a, b) {
-    const dateA = Date.parse(a.split('utc=')[1]);
-    const dateB = Date.parse(b.split('utc=')[1]);
+    const dateA = Date.parse(a.trim().split('utc=')[1]);
+    const dateB = Date.parse(b.trim().split('utc=')[1]);
 
     return dateB < dateA ? 1 : -1;
   });
