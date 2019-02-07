9567fc6ae0028639bac0797f997dd08ab996d6ff
Marco Azzalin <marco.azzalin@potatolondon.com>
2019-02-07 11:28:00 +0000

    [touch:6]Create constants for tenants keys

--- src/core/settings/tenants.py
+++ src/core/settings/tenants.py
@@ -1,11 +1,15 @@
+ADS = 'ads'
+PUBLISHERS = 'publishers'
+
+
 TENANTS = {
-    'ads': {
+    ADS: {
         'label': 'Advertise',
         'QUALTRICS_SURVEY_ID': 'SV_beH0HTFtnk4A5rD',
         'EMAIL_TO': 'Q97_4_TEXT',
         'EMAIL_BCC': 'Q97_5_TEXT',
     },
-    'publishers': {
+    PUBLISHERS: {
         'label': 'Publishers',
         'QUALTRICS_SURVEY_ID': None,
         'EMAIL_TO': None,
