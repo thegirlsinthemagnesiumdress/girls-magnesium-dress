c8ce9c8f89a487809a067a28e8903a334eccc154
Kevin Carrogan <kevinm@potatolondon.com>
2019-02-13 13:54:12 +0000

    [touch:7]Tidy process request for default tenant redirect middleware.

--- src/public/middleware.py
+++ src/public/middleware.py
@@ -1,18 +1,22 @@
 from django.core.urlresolvers import resolve
-from django.shortcuts import redirect
 from django.conf import settings
 from django.http import Http404
+from django.shortcuts import redirect
 from django.template.exceptions import TemplateDoesNotExist
 
 
 class RedirectToDefaultTenant(object):
 
     def process_request(self, request, *args, **kwargs):
-        view = resolve(request.path)
-        current_url = resolve(request.path_info).url_name
-        if current_url and 'legacy' in view.namespaces and not view.kwargs.get('tenant'):
-            view.kwargs.update({'tenant': settings.ADS})
-            return redirect(current_url, *view.args, **view.kwargs)
+        match = resolve(request.path)
+        url = match.url_name
+
+        is_match_in_legacy_namespace = 'legacy' in match.namespaces
+        has_tenant_kwarg = match.kwargs.get('tenant') is not None
+
+        if is_match_in_legacy_namespace and not has_tenant_kwarg:
+            match.kwargs.update({'tenant': settings.ADS})
+            return redirect(url, *match.args, **match.kwargs)
 
     def process_exception(self, request, exception):
         if isinstance(exception, TemplateDoesNotExist):
