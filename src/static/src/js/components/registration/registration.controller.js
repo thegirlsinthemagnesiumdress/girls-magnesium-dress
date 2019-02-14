a5bb57c81ef8295fde3e275139a53d6fae344fb9
Ahmed Abuelgasim <ahmed@potatolondon.com>
2019-02-14 14:36:36 +0000

    `/createsurvey` page will now send the tenant name, extracted from the URL, to BE as 'tenant' [touch:34]

--- src/static/src/js/components/registration/registration.controller.js
+++ src/static/src/js/components/registration/registration.controller.js
@@ -1,7 +1,7 @@
 goog.module('dmb.components.registration.controller');
 
 const SURVEY_ENDPOINT = '/api/survey';
-
+const CREATE_SURVEY = 'createsurvey';
 
 /**
  * Registration controller.
@@ -46,6 +46,15 @@ class RegistrationController {
 
     /**
      * @export
+     * @type{string}
+     *
+     * Get tenant from URL structure: url/<tenant>/createsurvey/
+     *
+     */
+    this.tenant = $location.absUrl().split(`/${CREATE_SURVEY}`)[0].split(`/`).pop();
+
+    /**
+     * @export
      * @type {boolean}
      */
     this.serverError = false;
@@ -73,7 +82,10 @@ class RegistrationController {
       'industry': this.industry,
       'country': this.country,
       'engagement_lead': this.engagementLead,
+      'tenant': this.tenant,
     };
+
+    console.log(data);
 
     if (this.elId && !data['engagement_lead']) {
       data['engagement_lead'] = this.elId;
