f8a9c0bf0eaf922a8e4b9c710e50a0cb6ef88b01
Paolo <paolo@potatolondon.com>
2019-03-04 14:17:58 +0000

    Create new url/view for new report page [touch: 83]
    Change-Id: Iaa2f16193a88f8f92a2552ff316c6843583d8a37

--- src/public/urls.py
+++ src/public/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from public.views import reports_admin, registration, report_static, index_static, result_detail, report_result_static
+from public.views import reports_admin, registration, report_static, index_static, result_detail, report_result_static, report_static_news
 
 urlpatterns = [
     url(r'^$', index_static, name="index"),
     url(r'^createsurvey/$', registration, name="registration"),
     url(r'^reports/$', reports_admin, name="reports"),
     url(r'^reports/(?P<sid>[\w]+)/$', report_static, name="report"),
+    url(r'^reports-news/(?P<sid>[\w]+)/$', report_static_news, name="report"),
     url(r'^reports/result/(?P<response_id>[\w]+)/$', report_result_static, name="report_result"),
     url(r'^result-detail/(?P<response_id>[\w]+)/$', result_detail, name="result-detail"),
 ]
