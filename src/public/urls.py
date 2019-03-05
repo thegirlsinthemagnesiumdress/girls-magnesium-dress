9f37f9642782ab718713aca9c6d4b982ba45b2c1
Paolo <paolo@potatolondon.com>
2019-03-05 11:39:20 +0000

    HOTFIX - Fix report url
    Change-Id: Ia33b2d20c6a6db856b63a5805c5043eb5792a0c8

--- src/public/urls.py
+++ src/public/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^createsurvey/$', registration, name="registration"),
     url(r'^reports/$', reports_admin, name="reports"),
     url(r'^reports/(?P<sid>[\w]+)/$', report_static, name="report"),
-    url(r'^reports-news/(?P<sid>[\w]+)/$', report_static_news, name="report"),
+    url(r'^reports-news/(?P<sid>[\w]+)/$', report_static_news, name="report-news"),
     url(r'^reports/result/(?P<response_id>[\w]+)/$', report_result_static, name="report_result"),
     url(r'^result-detail/(?P<response_id>[\w]+)/$', result_detail, name="result-detail"),
 ]
