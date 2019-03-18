ec650c38d84a9f4b9bc767042e2e8acb83dfbb27
Marco Azzalin <marco.azzalin@potatolondon.com>
2019-03-18 17:15:02 +0000

    update settings and dimensions [touch:181]

--- src/core/settings/tenants/retail.py
+++ src/core/settings/tenants/retail.py
@@ -2,6 +2,15 @@ WEIGHTS = {}
 
 DIMENSION_TITLES = {}
 
-DIMENSIONS = {}
+DIMENSIONS = {
+    'strategic_direction': 'Strategic direction and data foundations',
+    'user_experience': 'User Experience',
+    'core_sales': 'Core Sales Activities',
+    'monetization_opportunities': 'Emerging Monetization Opportunities',
+}
 
 MULTI_ANSWER_QUESTIONS = []
+
+DIMENSIONS_WEIGHTS_QUESTION_ID = None
+
+DIMENSIONS_WEIGHTS = {}
