8ebf3fbb597e4578d1d30ce1198ffaecaf367327
Ahmed Abuelgasim <ahmed@potatolondon.com>
2019-03-20 14:21:21 +0000

    - added industry not available - improved accessibility [touch:84]

--- src/static/src/js/components/progress-grid/progress-grid.template.js
+++ src/static/src/js/components/progress-grid/progress-grid.template.js
@@ -5,7 +5,11 @@ const template = `
     class="dmb-progress-grid dmb-progress-grid--level-{[progressGridCtrl.floorDmbFactory_(ratingMain)]}"
     ng-class="{
       'dmb-progress-grid--overflow-v': progressGridCtrl.verticalOverflow,
-      'dmb-progress-grid--overflow-h': progressGridCtrl.horizontalOverflow}">
+      'dmb-progress-grid--overflow-h': progressGridCtrl.horizontalOverflow}"
+    aria-label="
+      Your company rating is {[ratingMain|number:1]}.
+      The industry average is {[industryAvg|number:1]}.
+      The industry best is {[industryBest|number:1]}.">
 
   <!-- col-bg -->
   <div
@@ -16,15 +20,20 @@ const template = `
   <!-- grid headings -->
   <div
       class="dmb-progress-grid__heading dmb-progress-grid__heading--level-{[$index]} h-c-eyebrow"
-      ng-repeat="level in progressGridCtrl.levels">
+      ng-repeat="level in progressGridCtrl.levels"
+      aria-hidden="true">
     {[level]}
   </div>
 
   <!-- bar labels -->
-  <div class="dmb-progress-grid__label dmb-progress-grid__label--company h-u-font-weight-medium">
+  <div
+      class="dmb-progress-grid__label dmb-progress-grid__label--company h-u-font-weight-medium"
+      aria-hidden="true">
     {[companyName]}
   </div>
-  <div class="dmb-progress-grid__label dmb-progress-grid__label--ind-avg h-u-font-weight-regular">
+  <div
+      class="dmb-progress-grid__label dmb-progress-grid__label--ind-avg h-u-font-weight-regular"
+      aria-hidden="true">
     <button
         dmb-side-panel-trigger="#dmb-report-left-side-panel-two"
         aria-label="Get more informations about the industry average">
@@ -33,7 +42,9 @@ const template = `
         </svg>
     </button> Industry average
   </div>
-  <div class="dmb-progress-grid__label dmb-progress-grid__label--ind-best h-u-font-weight-regular">
+  <div
+      class="dmb-progress-grid__label dmb-progress-grid__label--ind-best h-u-font-weight-retegular"
+      aria-hidden="true">
     <button
         dmb-side-panel-trigger="#dmb-report-left-side-panel-two"
         aria-label="Get more informations about the industry best">
@@ -78,6 +89,18 @@ const template = `
       class="dmb-progress-grid__h-bar dmb-progress-grid__h-bar--ind-best"
       data-value="{[industryBest|number:1]}"
       ng-style="{width: progressGridCtrl.getProgress(industryBest)}">
+  </div>
+
+  <!-- "Not available" banners -->
+  <div
+      class="dmb-progress-grid__banner dmb-progress-grid__banner--ind-avg h-c-footnote"
+      ng-if="industryReady && !industryAvg">
+    Industry average is not available yet, sorry!
+  </div>
+  <div
+      class="dmb-progress-grid__banner dmb-progress-grid__banner--ind-best h-c-footnote"
+      ng-if="industryReady && !industryBest">
+    Industry best is not available yet, sorry!
   </div>
 </div>
 `;
