454a928410db932e7e9ca2e660be6f283509f309
Ahmed Abuelgasim <ahmed@potatolondon.com>
2019-03-20 17:13:35 +0000

    improved accessibility further [touch:84]

--- src/static/src/js/components/progress-grid/progress-grid.template.js
+++ src/static/src/js/components/progress-grid/progress-grid.template.js
@@ -5,11 +5,33 @@ const template = `
     class="dmb-progress-grid dmb-progress-grid--level-{[progressGridCtrl.floorDmbFactory_(ratingMain)]}"
     ng-class="{
       'dmb-progress-grid--overflow-v': progressGridCtrl.verticalOverflow,
-      'dmb-progress-grid--overflow-h': progressGridCtrl.horizontalOverflow}"
-    aria-label="
-      Your company rating is {[ratingMain|number:1]}.
-      The industry average is {[industryAvg|number:1]}.
-      The industry best is {[industryBest|number:1]}.">
+      'dmb-progress-grid--overflow-h': progressGridCtrl.horizontalOverflow
+    }">
+
+    <ul class="dmb-u-screen-reader-only">
+      <li>
+        <span ng-if="!ratingMain">
+          Fetching your company rating...
+        </span>
+        <span ng-if="ratingMain">
+          Your company, {[companyName]}, has a rating of {[ratingMain|number:1]}, which is a maturity level of {[progressGridCtrl.getLevelName(ratingMain)]}.
+        </span>
+      </li>
+        <span ng-if="!industryReady">
+          Fetching industry average...
+        </span>
+        <span ng-if="industryReady">
+          The industry average is {[industryAvg|number:1]}, which is a maturity level of {[progressGridCtrl.getLevelName(industryAvg)]}.
+        </span>
+        <li>
+        <span ng-if="!industryReady">
+          Fetching industry best...
+        </span>
+        <span ng-if="industryReady">
+          The industry best is {[industryBest|number:1]}, which is a maturity level of {[progressGridCtrl.getLevelName(industryBest)]}.
+        </span>
+      </li>
+    </ul>
 
   <!-- col-bg -->
   <div
@@ -26,32 +48,32 @@ const template = `
   </div>
 
   <!-- bar labels -->
-  <div
-      class="dmb-progress-grid__label dmb-progress-grid__label--company h-u-font-weight-medium"
-      aria-hidden="true">
+  <div class="dmb-progress-grid__label dmb-progress-grid__label--company h-u-font-weight-medium">
     {[companyName]}
   </div>
   <div
       class="dmb-progress-grid__label dmb-progress-grid__label--ind-avg h-u-font-weight-regular"
-      aria-hidden="true">
+      aria-label="{[progressGridCtrl.indAvgAriaLabel]}">
     <button
         dmb-side-panel-trigger="#dmb-report-left-side-panel-two"
-        aria-label="Get more informations about the industry average">
+        aria-label="Get more information about the industry average"
+        aria-controls="dmb-rerort-left-side-panel-two"
+        aria-expanded="false">
        <svg role="img" class="dmb-svg dmb-svg--info" width="20px" height="20px">
           <use xlink:href="#info"></use>
         </svg>
-    </button> Industry average
+    </button>
+    <span aria-hidden="true">Industry average</span>
   </div>
-  <div
-      class="dmb-progress-grid__label dmb-progress-grid__label--ind-best h-u-font-weight-regular"
-      aria-hidden="true">
+  <div class="dmb-progress-grid__label dmb-progress-grid__label--ind-best h-u-font-weight-regular">
     <button
         dmb-side-panel-trigger="#dmb-report-left-side-panel-two"
-        aria-label="Get more informations about the industry best">
+        aria-label="Get more information about the industry best">
        <svg role="img" class="dmb-svg dmb-svg--info" width="20px" height="20px">
           <use xlink:href="#info"></use>
         </svg>
-    </button> Industry best
+    </button>
+    <span aria-hidden="true">Industry best</span>
   </div>
 
   <!-- vertical bars -->
@@ -59,36 +81,42 @@ const template = `
       class="dmb-progress-grid__v-bar dmb-progress-grid__v-bar--company"
       data-value="{[ratingMain|number:1]}"
       data-level-name="{[progressGridCtrl.getLevelName(ratingMain)]}"
-      ng-style="{height: progressGridCtrl.getProgress(ratingMain)}">
+      ng-style="{height: progressGridCtrl.getProgress(ratingMain)}"
+      aria-hidden="true">
   </div>
   <div
       class="dmb-progress-grid__v-bar dmb-progress-grid__v-bar--ind-avg"
       data-value="{[industryAvg|number:1]}"
       data-level-name="{[progressGridCtrl.getLevelName(industryAvg)]}"
-      ng-style="{height: progressGridCtrl.getProgress(industryAvg)}">
+      ng-style="{height: progressGridCtrl.getProgress(industryAvg)}"
+      aria-hidden="true">
   </div>
   <div
       class="dmb-progress-grid__v-bar dmb-progress-grid__v-bar--ind-best"
       data-value="{[industryBest|number:1]}"
       data-level-name="{[progressGridCtrl.getLevelName(industryBest)]}"
-      ng-style="{height: progressGridCtrl.getProgress(industryBest)}">
+      ng-style="{height: progressGridCtrl.getProgress(industryBest)}"
+      aria-hidden="true">
   </div>
 
   <!-- horizontal bars -->
   <div
       class="dmb-progress-grid__h-bar dmb-progress-grid__h-bar--company"
       data-value="{[ratingMain|number:1]}"
-      ng-style="{width: progressGridCtrl.getProgress(ratingMain)}">
+      ng-style="{width: progressGridCtrl.getProgress(ratingMain)}"
+      aria-hidden="true">
   </div>
   <div
       class="dmb-progress-grid__h-bar dmb-progress-grid__h-bar--ind-avg"
       data-value="{[industryAvg|number:1]}"
-      ng-style="{width: progressGridCtrl.getProgress(industryAvg)}">
+      ng-style="{width: progressGridCtrl.getProgress(industryAvg)}"
+      aria-hidden="true">
   </div>
   <div
       class="dmb-progress-grid__h-bar dmb-progress-grid__h-bar--ind-best"
       data-value="{[industryBest|number:1]}"
-      ng-style="{width: progressGridCtrl.getProgress(industryBest)}">
+      ng-style="{width: progressGridCtrl.getProgress(industryBest)}"
+      aria-hidden="true">
   </div>
 
   <!-- "Not available" banners -->
