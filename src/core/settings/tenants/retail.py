cf60e2b4b80072ab342e828762cfb5d403b7d807
Paolo <paolo@potatolondon.com>
2019-03-20 15:08:03 +0000

    Fix dimension settings [touch: 186]
    Change-Id: I6122f3409824a3691bf9e386a55996bfb0d0c7cd

--- src/core/settings/tenants/retail.py
+++ src/core/settings/tenants/retail.py
@@ -8,10 +8,10 @@ DIMENSION_TITLES = {
 }
 
 DIMENSIONS = {
-    'strategic_direction': 'Strategic direction and data foundations',
-    'user_engagement': 'User engagement',
-    'core_sales': 'Core sales activities',
-    'emerging_monetization': 'Emerging monetization opportunities',
+    'strategic_direction': [],
+    'user_engagement': [],
+    'core_sales': [],
+    'emerging_monetization': [],
 }
 
 MULTI_ANSWER_QUESTIONS = []
