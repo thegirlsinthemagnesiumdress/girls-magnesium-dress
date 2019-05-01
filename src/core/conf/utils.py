97a674799622147c2dc38d0ea703ba856ebaa417
Marco Azzalin <marco.azzalin@potatolondon.com>
2019-05-01 15:23:43 +0100

    Add staging test

--- src/core/conf/utils.py
+++ src/core/conf/utils.py
@@ -124,7 +124,7 @@ def version_info(domain):
     """
     version = None
     is_nightly = False
-    if not environment.is_production_environment():
+    if not environment.is_production_environment() and "staging" in environment.application_id():
         version_match = VERSION_RE.search(domain)
         if version_match:
             version = version_match.group('version')
