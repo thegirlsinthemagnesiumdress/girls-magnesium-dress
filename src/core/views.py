7bbd3fa5029e46125e2c32f649dda1b7be353fb5
Marco Azzalin <marco.azzalin@potatolondon.com>
2019-06-04 15:41:41 +0200

    Removed unused import #402

--- src/core/views.py
+++ src/core/views.py
@@ -8,7 +8,6 @@ from djangae.environment import task_or_admin_only
 from core.management import migrations
 from core.tasks import sync_qualtrics, generate_csv_export, calculate_industry_benchmark
 from django.conf import settings
-from core.models import Survey
 from django.shortcuts import render
 
 
