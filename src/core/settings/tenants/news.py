2c3b8fc8d34cb9b85f0826a08024c12943243e32
Ahmed Abuelgasim <ahmed@potatolondon.com>
2019-06-11 17:34:50 +0100

    added level logic changes to other tenants #392

--- src/core/settings/tenants/news.py
+++ src/core/settings/tenants/news.py
@@ -489,6 +489,7 @@ DIMENSION_SIDEPANEL_DESCRIPTIONS = {
 
 CONTENT_DATA = {
     'levels': LEVELS,
+    'levels_max': LEVELS_MAX,
     'level_descriptions': LEVELS_DESCRIPTIONS,
     'report_level_descriptions': REPORT_LEVEL_DESCRIPTIONS,
     'dimensions': DIMENSION_ORDER,
