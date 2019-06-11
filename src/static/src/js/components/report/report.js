e7a27c8e56fd88398503554daaf9c8b3835863fe
Ahmed Abuelgasim <ahmed@potatolondon.com>
2019-06-11 15:20:47 +0100

    added `dmbLevelsFactory` and `resultInTopLevel` factories #392

--- src/static/src/js/components/report/report.js
+++ src/static/src/js/components/report/report.js
@@ -17,6 +17,65 @@ const module = angular.module(MODULE_NAME, []);
 module.directive(directive.DIRECTIVE_NAME, directive.main);
 module.service(service.SERVICE_NAME, service.main);
 
+module.factory('dmbLevelsFactory', ['tenantConf', (tenantConf) => {
+  // Factory that takes a non-integer number and an optional map of strings
+  // and returns the current level value (minimum number in range),
+  // the next level value (minimum number in range)
+  // and the value corresponding to the nearest key in the map
+  return (value, sourceMap) => {
+    if (!angular.isDefined(value)) {
+      return '';
+    }
+
+    if (!sourceMap) {
+      // Default to `tenantConf.levels` if no map given
+      sourceMap = tenantConf.levels;
+    }
+
+    const sourceKeys = Object.keys(sourceMap).sort();
+    for (const [index, val] of sourceKeys.entries()) {
+      // If last element
+      if (index == sourceKeys.length - 1) {
+        return {
+          current: {
+            value: val,
+            mapValue: sourceMap[val],
+          },
+          next: {
+            value: val,
+            mapValue: sourceMap[val],
+          },
+        };
+      } else if (value >= val && value < sourceKeys[index + 1]) {
+        const nextLevel = sourceKeys[index + 1];
+        // Including the return twice for early break when condition met
+        return {
+          current: {
+            value: val,
+            mapValue: sourceMap[val],
+          },
+          next: {
+            value: nextLevel,
+            mapValue: sourceMap[nextLevel],
+          },
+        };
+      }
+    }
+  };
+}]);
+
+module.factory('resultInTopLevel', ['tenantConf', (tenantConf) => {
+  return (value) => {
+    if (!angular.isDefined(value)) {
+      return '';
+    }
+
+    const levelsArray = Object.keys(tenantConf.levels).sort();
+    return value >= levelsArray[levelsArray.length - 1];
+  };
+}]);
+
+
 module.factory('floorDmbFactory', ['tenantConf', (tenantConf) => {
   const levelsTotal = tenantConf.levelsTotal;
   return (dmb) =>
