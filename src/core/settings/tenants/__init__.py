8afcafb7f4c39f65926fcbadd347d964fb6b616c
Marco Azzalin <marco.azzalin@potatolondon.com>
2019-07-03 13:51:09 +0100

    Rename _TENANTS to INTERNAL_TENANTS, and fix staging settings

--- src/core/settings/tenants/__init__.py
+++ src/core/settings/tenants/__init__.py
@@ -34,7 +34,7 @@ RETAIL = 'retail'
 CLOUD = 'cloud'
 
 
-_TENANTS = {
+INTERNAL_TENANTS = {
     ADS: {
         'key': ADS,
         'label': _('Advertisers'),
@@ -139,7 +139,7 @@ DEFAULT_TENANT = ADS
 
 
 def active_tenants():
-    return {k: v for k, v in _TENANTS.items() if v['enabled']}
+    return {k: v for k, v in INTERNAL_TENANTS.items() if v['enabled']}
 
 TENANTS = active_tenants()
 
