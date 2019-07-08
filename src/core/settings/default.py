86b8ef44e44bc5949bf9020ce267d2f097fa0b10
Alex Eales <alexeales@Stephanies-MBP.localdomain>
2019-07-08 15:46:04 +0100

    Fixed typo in middleware definition

--- src/core/settings/default.py
+++ src/core/settings/default.py
@@ -76,7 +76,7 @@ MIDDLEWARE_CLASSES = (
     'djangae.contrib.common.middleware.RequestStorageMiddleware',
     'google.appengine.ext.appstats.recording.AppStatsDjangoMiddleware',
     'public.middleware.RedirectToDefaultTenant',
-    'public.middleware.ForceDefaults',
+    'public.middleware.ForceDefaultLang',
 )
 
 STATICFILES_FINDERS = (
