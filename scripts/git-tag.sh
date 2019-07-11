873f94482bf16d668d1d1f70cd50e6d6106ec5ee
Ahmed Abuelgasim <ahmed@potatolondon.com>
2019-07-11 14:30:40 +0100

    removed scripts #515

--- scripts/git-tag.sh
+++
@@ -1,7 +0,0 @@
-# GIT TAG SCRIPT
-#!/bin/bash
-echo "Latest tag: $(git describe --tags $(git rev-list --tags --max-count=1))"
-read -p "Enter new tag: " TAG
-echo "Tagging with $TAG";
-git tag $TAG
-git push --tags
