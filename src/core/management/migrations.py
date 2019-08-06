3ebc023239257851ea3edac9e9d3a1aebcf05c4f
Alex Eales <alexeales@Stephanies-MBP.localdomain>
2019-08-06 14:42:24 +0100

    Enable/disable cloud during migration

--- src/core/management/migrations.py
+++ src/core/management/migrations.py
@@ -54,9 +54,19 @@ def migrate_to_tenant(old_tenant, new_tenant):
 
 
 def migrate_to_dmblite_survey():
-    # Ensure that all tenants with stored surveys are enabled before running this migration
+    # Enable all tenants since the Survey.save method will
+    # error if a Survey is using a disabled tenant
+    logging.info("Enabling cloud tenant")
+    settings.ALL_TENANTS['cloud']['enabled'] = True
+    settings.TENANTS = {k: v for k, v in settings.ALL_TENANTS.items() if v['enabled']}
+
     logging.info("Adding DMBLite fields to surveys")
     surveys = Survey.objects.all()
     logging.info("Found {} surveys to be converted".format(surveys.count()))
     for survey in surveys:
         survey.save()
+
+    # Disable all tenants after task
+    logging.info("Disabling cloud tenant")
+    settings.ALL_TENANTS['cloud']['enabled'] = False
+    settings.TENANTS = {k: v for k, v in settings.ALL_TENANTS.items() if v['enabled']}
