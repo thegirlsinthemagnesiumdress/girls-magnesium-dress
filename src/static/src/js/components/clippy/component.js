e5730f0e6397f8e5270312bbf9b741080afed82b
Alex Eales <alex.eales@potatolondon.com>
2019-08-21 10:59:09 +0100

    Removed redundant constructor

--- src/static/src/js/components/clippy/component.js
+++ src/static/src/js/components/clippy/component.js
@@ -13,27 +13,19 @@ import ClipboardJs from 'clipboard';
  */
 export default class Clippy {
   /**
-   *  Constructor for the clippy module.
-   * @param {Node} elem : Element in the dom to attach event listeners etc. to.
+   * Attachs clippy functionality to element.
+   * @param {Node} elem : Element to attach Clippy to.
    */
-  constructor(elem) {
-    this.root = elem;
-    this.clipboard = new ClipboardJs(elem, {
+  static attachTo(elem) {
+    // Create new clipboard JS object which gets the value of the dmb-clipp attribute.
+    let clipboard = new ClipboardJs(elem, {
       text: (trigger) => {
         return trigger.getAttribute('dmb-clippy');
       },
     });
-    this.clipboard.on('success', (e) => {
+    // On success clear the selection and TODO: show a toast notification.
+    clipboard.on('success', (e) => {
       e.clearSelection();
     });
-  }
-
-  /**
-   * Attachs clippy functionality to element.
-   * @param {Node} elem : Element to attach Clippy to.
-   * @return {Clippy} : returns a clippy instance for the element.
-   */
-  static attachTo(elem) {
-    return new Clippy(elem);
   }
 }
