027228dc4a710e6cc98f31b3947d21a7660f2650
Alex Eales <alex.eales@potatolondon.com>
2019-08-28 14:34:40 +0100

    Added pre-defined scores to sample surveys

--- src/core/management/commands/create_sample_survey.py
+++ src/core/management/commands/create_sample_survey.py
@@ -4,8 +4,37 @@ from django.core.management.base import BaseCommand
 from core.tests.mommy_recepies import make_survey, make_survey_result
 
 import numpy
-import random
 import logging
+
+
+SAMPLE_DMB_D = {
+    'ads': {
+        "organization": 2.1,
+        "attribution": 1.6,
+        "access": 2.3,
+        "ads": 3.7,
+        "audience": 0.6,
+        "automation": 2.0
+    },
+    'retail': {
+        "emerging_monetization": 1.6,
+        "user_engagement": 2.0,
+        "core_sales": 3.0,
+        "strategic_direction": 0.95
+    },
+    'news': {
+        "reader_revenue": 1.2,
+        "reader_engagement": 2.8,
+        "strategic_direction": 3.5,
+        "advertising_revenue": 0.9
+    },
+    'cloud': {
+        "learn": 1.2,
+        "lead": 0.8,
+        "scale": 3.5,
+        "secure": 2.9
+    }
+}
 
 
 class Command(BaseCommand):
@@ -18,9 +47,7 @@ class Command(BaseCommand):
             with transaction.atomic(xg=True):
                 survey = make_survey(company_name='ACME Corp.', tenant=tenant)
                 # Create sample survey result
-                dimensions = tenants[tenant]['CONTENT_DATA']['dimensions']
-                max_level = tenants[tenant]['CONTENT_DATA']['levels_max']
-                dmb_d = {d: random.random() * max_level for d in dimensions}
+                dmb_d = SAMPLE_DMB_D[tenant]
                 dmb = numpy.average(dmb_d.values())
                 result = make_survey_result(survey=survey, dmb=dmb, dmb_d=dmb_d)
                 # Explicitly links the result to the survey as this isn't always completed.
