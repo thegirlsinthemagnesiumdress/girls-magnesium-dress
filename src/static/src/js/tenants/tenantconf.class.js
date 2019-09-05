b83d1cab398cff67d86ebb01bc8456561a3e19a5
Michael Strutt <mstrutt@potatolondon.com>
2019-09-05 14:59:27 +0100

    Creating vanilla tennant conf to use
    Change-Id: I8009a6a79ed5f5c963c8b090d5a516491afbbaec

---
+++ src/static/src/js/tenants/tenantconf.class.js
@@ -0,0 +1,188 @@
+const tenantDataElementName = 'bootstrap-data';
+
+/**
+ * Tenant conf class
+ */
+export class TenantConfiguration {
+  /**
+   *
+   */
+  constructor() {
+    /**
+     * @type string
+     * @export
+     */
+    this.tenant = '';
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.levels = {};
+
+    /**
+     * @type {string}
+     * @export
+     */
+    this.levelsMax = '';
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.levelDescriptions = {};
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.reportLevelDescriptions = {};
+
+    /**
+     * @type {Array}
+     * @export
+     */
+    this.dimensionList = [];
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.dimensionTitles = {};
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.dimensionHeaderDescriptions = {};
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.dimensionLevelDescription = {};
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.dimensionRecommendations = {};
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.industryAvgDescription = null;
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.industryBestDescription = null;
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.contentData = {};
+
+    /**
+     * @type {string}
+     * @export
+     */
+    this.dimensionSidepanelHeading = '';
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.subdimensions = {};
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.subdimensionLists = {};
+
+    /**
+     * @type {string}
+     * @export
+     */
+    this.subdimensionsReportHeading = '';
+
+    /**
+     * @type {string}
+     * @export
+     */
+    this.subdimensionsReportDescription = '';
+
+    /**
+     * @type {string}
+     * @export
+     */
+    this.subdimensionsReportCta = '';
+
+    /**
+     * @type {string}
+     * @export
+     */
+    this.subdimensionsReportSubheading = '';
+
+    /**
+     * @type {string}
+     * @export
+     */
+    this.subdimensionsSidepanelHeading = '';
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.subdimensionNames = {};
+
+    /**
+     * @type {Object}
+     * @export
+     */
+    this.subdimensionDescriptions = {};
+
+
+    const tenantDataElement = document.getElementById(tenantDataElementName);
+    this.tenant = tenantDataElement.dataset['tenant'];
+
+    this.contentData = JSON.parse(tenantDataElement.dataset['contentData']);
+
+    this.levels = this.contentData['levels'];
+    this.levelsMax = this.contentData['levels_max'];
+    const levelKeys = Object.keys(this.levels);
+    this.levelsTotal = levelKeys.length;
+    this.levelsArray = levelKeys.sort();
+    this.levelDescriptions = this.contentData['level_descriptions'];
+    this.reportLevelDescriptions = this.contentData['report_level_descriptions'];
+
+    this.dimensionList = this.contentData['dimensions'];
+    this.dimensionTitles = this.contentData['dimension_titles'];
+    this.dimensionHeaderDescriptions = this.contentData['dimension_header_descriptions'];
+    this.dimensionLevelDescription = this.contentData['dimension_level_description'];
+    this.dimensionRecommendations = this.contentData['dimension_recommendations'];
+    this.industryAvgDescription = this.contentData['industry_avg_description'];
+    this.industryBestDescription = this.contentData['industry_best_description'];
+    this.dimensionSidepanelHeading = this.contentData['dimension_sidepanel_heading'];
+    this.dimensionSidepanelDescriptions = this.contentData['dimension_sidepanel_descriptions'];
+
+    this.subdimensionLists = this.contentData['subdimension_lists'];
+    if (this.subdimensionLists) {
+      this.subdimensionsReportHeading = this.contentData['subdimensions_report_heading'];
+      this.subdimensionsReportSubheading = this.contentData['subdimensions_report_subheading'];
+      this.subdimensionsReportDescription = this.contentData['subdimensions_report_description'];
+      this.subdimensionsReportCta = this.contentData['subdimensions_report_cta'];
+      this.subdimensionsSidepanelHeading = this.contentData['subdimensions_sidepanel_heading'];
+
+      this.subdimensionNames = this.contentData['subdimension_names'];
+      this.subdimensionDescriptions = this.contentData['subdimension_descriptions'];
+    }
+  }
+}
+
+// Default export is a single instance of this so that it's available for all vanilla files
+export default new TenantConfiguration();
